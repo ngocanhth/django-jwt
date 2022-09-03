ee1e230c1e1c5a95cafa0c263fa321745d36d097
ngocanh <lengocanhxd@gmail.com>
2022-09-03 13:33:02 +0700

    Meta option django

--- ecourses/courses/models.py
+++ ecourses/courses/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from email.policy import default
 from pydoc import describe
 from xml.parsers.expat import model
 from django.db import models
@@ -8,15 +9,28 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='uploads/%Y/%n')
-    
+
 class Category(models.Model):
     name = models.CharField(max_length=100, null=False, unique=True)
-    
-class Course (models.Model):
+class ItemBase(models.Model):
+    class Meta:
+        abstract = True
+        
     subject = models.CharField(max_length=255, null=False)
-    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='courses/%Y/%m', default=None)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+class Course (ItemBase):
+    class Meta:
+        unique_together = ('subject', 'category') #trong 1 danh muc ko dc phep trung ten khoa hoc subject 
+        ordering =["-id"] # sap xep tang dan theo id, ["-id"] giam dan theo id
+       # ordering =["subject", "created_date"]
+    description = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+class Lesson(ItemBase):
+    class Meta:
+        unique_together = ('subject', 'course') #trong 1 khoa hoc ko dc phep trung ten bai hoc subject 
+    content = models.TextField(null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     
--- ecourses/ecourses/settings.py
+++ ecourses/ecourses/settings.py
@@ -78,12 +78,17 @@ WSGI_APPLICATION = 'ecourses.wsgi.application'
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'ecoursesdb',
+    #     'NAME': 'ecourses',
+    #     'OPTIONS': {
+    #         'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+    #         'charset':'utf8mb4',
+    #     },
     #     'USER': 'root',
     #     'PASSWORD': '',
     #     'HOST': '127.0.0.1',
     #     'PORT': '3306',
-    # }
+    # },
+        
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
